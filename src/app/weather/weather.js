8b8a6dcb806784a23957bdf775ad0bec77e65bf2
= <humer015@umn.edu>
2015-05-08 23:53:21 -0500

    hourly"

--- src/app/weather/weather.js
+++ src/app/weather/weather.js
@@ -17,6 +17,7 @@ angular.module('myApp.weather', [
 		weather.longitude = '';
 
 		weather.getCurrentWeatherByLatLon = function(latitude, longitude) {
+			weather.selection = 'currently';
 			$http.get('/currently?latitude=' + latitude + '&longitude=' + longitude)
 				.success(function(data, status, headers, config) {
 					weather.data = data; // this callback will be called asynchronously
@@ -50,7 +51,7 @@ angular.module('myApp.weather', [
 		// 		});
 		// };
 		weather.getHourWeatherByLatLon = function(latitude, longitude) {
-
+			weather.selection = 'hourly';
 			$http.get('/hourly?latitude=' + latitude + '&longitude=' + longitude)
 				.success(function(data, status, headers, config) {
 					weather.data = data.hourly.data; // this callback will be called asynchronously
@@ -72,10 +73,20 @@ angular.module('myApp.weather', [
 				});
 		};
 		weather.getDayWeatherByLatLon = function(latitude, longitude) {
+			weather.selection = 'daily';
 			$http.get('/daily?latitude=' + latitude + '&longitude=' + longitude)
 				.success(function(data, status, headers, config) {
-					weather.data = data; // this callback will be called asynchronously
-					// when the response is available
+					weather.data = data.daily.data; // this callback will be called asynchronously
+					$timeout(function() {
+						var skycons = new Skycons({
+							"color": "red"
+						});
+
+						for (var i = 0; i < weather.data.length; i++) {
+							skycons.add("weather_icon_" + i, weather.data[i].icon);
+						}
+						skycons.play();
+					}, 500);
 				})
 				.error(function(data, status, headers, config) {
 					weather.error = status;
@@ -110,19 +121,86 @@ angular.module('myApp.weather', [
 				'<span>Pressure: {{hour.pressure}} </span><br />' +
 				'</div>',
 			link: function(scope) {
-				console.log("In link");
-				console.log(scope)
-				console.log(scope.$parent.$index)
-				console.log(scope.$index)
 				scope.convertTime = $sce.trustAsHtml('Time: ' + timeFactory.convertTime(scope.weather.data[scope.$parent.$index].time));
 			}
 		}
 	})
+	.directive('daily', function(timeFactory, $sce) {
+		return {
+			scope: true,
+			restrict: 'E',
+			template: '<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4" ng-bind-html="convertedDay">{{convertedDay}}</div>' +
+				'<div class="weather_block daily col-xs-8 col-sm-8 col-md-8 col-lg-8">' +
+				'<canvas id="weather_icon_{{$index}}" width="128" height="128"></canvas><br />' +
+				'<span>Low: {{day.temperatureMin}} - Feels like: {{day.apparentTemperatureMin}}</span><br />' +
+				'<span>High: {{day.temperatureMax}} - Feels like: {{day.apparentTemperatureMax}} </span><br />' +
+				'<span> {{day.summary}} </span><br />' +
+				'<span>Chance of Percipitation: {{day.precipProbability}} </span><br />' +
+				'<br />' +
+				'<span>Sunrise: {{sunrise}} </span><br />' +
+				'<span>Sunset: {{sunset}} </span><br />' +
+				'<span>Moonphase: {{day.moonPhase}}</span>' +
+				'</div>',
+			link: function(scope) {
+				scope.sunrise = $sce.trustAsHtml(timeFactory.convertToHrMin(scope.day.sunriseTime));
+				scope.sunset = $sce.trustAsHtml(timeFactory.convertToHrMin(scope.day.sunsetTime));
+				scope.convertedDay = $sce.trustAsHtml(timeFactory.convertDay(scope.day.sunsetTime));
+			}
+		}
+
+	})
 	.factory('timeFactory', function() {
 		timeFactory = {};
 
+		timeFactory.convertDay = function(time) {
+			var d = new Date(time * 1000);
+
+			var month = new Array();
+			month[0] = "January";
+			month[1] = "February";
+			month[2] = "March";
+			month[3] = "April";
+			month[4] = "May";
+			month[5] = "June";
+			month[6] = "July";
+			month[7] = "August";
+			month[8] = "September";
+			month[9] = "October";
+			month[10] = "November";
+			month[11] = "December";
+
+			var day = new Array();
+			day[0] = "Sunday";
+			day[1] = "Monday";
+			day[2] = "Tuesday";
+			day[3] = "Wednesday";
+			day[4] = "Thursday";
+			day[5] = "Friday";
+			day[6] = "Saturday";
+
+			return day[d.getDay()] + " " + month[d.getMonth()] + " " + d.getDate()
+		};
+
+		timeFactory.convertToHrMin = function(time) {
+			var d = new Date(time * 1000);
+			var hours = d.getHours()
+			var minutes = d.getMinutes()
+
+			if (minutes < 10)
+				minutes = "0" + minutes
+
+			var suffix = "AM";
+			if (hours >= 12) {
+				suffix = "PM";
+				hours = hours - 12;
+			}
+			if (hours == 0) {
+				hours = 12;
+			}
+			return hours + ":" + minutes + " " + suffix
+		};
+
 		timeFactory.convertTime = function(time) {
-			console.log("in convert time.  Time: " + time);
 
 			var d = new Date(time * 1000);
 			var hours = d.getHours()
@@ -169,39 +247,40 @@ angular.module('myApp.weather', [
 		return timeFactory;
 	})
 	.service('locationService', function($http) {
-			var locationService = {}
-			locationService.getLocation = function(address) {
-				$http.get('/location?address=' + address)
-					.success(function(data, status, headers, config) {
-						return data;
-					})
-					.error(function(data, status, headers, config) {
+		var locationService = {}
+		locationService.getLocation = function(address) {
+			$http.get('/location?address=' + address)
+				.success(function(data, status, headers, config) {
+					return data;
+				})
+				.error(function(data, status, headers, config) {
 
-					})
+				})
 
-			}
-			return locationService
-		};
-
-
-		function handleError($window, status) {
-			if (status == 400) {
-				console.log('in 400');
-				//location.href = '/public/400.html'
-				$window.location.href = '/public/400.html';
-				$location.path('/public/400.html');
-			}
 		}
+		return locationService
 
-		// function createCurrentlyData(data) {
-		// 	var image = '';
+	});
 
-		// 	<div class="weather_block currently">
-		// 		<canvas id="weather_icon" width="128" height="128"></canvas>
-		// 		<span>Temp: {{data.currently.temperature}}  </span>
-		// 		<span> {{data.currently.summary}} </span>
-		// 		<span>Humidity: {{data.currently.humidity}} </span>
-		// 		<span>Pressure: {{data.currently.pressure}} </span>
-		// 	</div>
 
-		// }
+function handleError($window, status) {
+	if (status == 400) {
+		console.log('in 400');
+		//location.href = '/public/400.html'
+		$window.location.href = '/public/400.html';
+		$location.path('/public/400.html');
+	}
+}
+
+// function createCurrentlyData(data) {
+// 	var image = '';
+
+// 	<div class="weather_block currently">
+// 		<canvas id="weather_icon" width="128" height="128"></canvas>
+// 		<span>Temp: {{data.currently.temperature}}  </span>
+// 		<span> {{data.currently.summary}} </span>
+// 		<span>Humidity: {{data.currently.humidity}} </span>
+// 		<span>Pressure: {{data.currently.pressure}} </span>
+// 	</div>
+
+// }
